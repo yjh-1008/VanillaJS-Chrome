ff15739363279ccc9bc3ef5efd5e76d87af4e720
yjh-1008 <71313511+yjh-1008@users.noreply.github.com>
2021-08-13 02:21:51 +0900

    use query

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-const age= parseInt(prompt("How old are you?"));
-if(isNaN(age)){
-    console.log("Please write a number");
-}else if(age<18){
-    console.log("You are too young");
-}else if(age>21 && age<=50){
-    console.log("You can drink")
-}else if(age>50 && age<=80){
-    console.log("You should exercise");
-
-}else if(age>80){
-    console.log("You can do whatever you want");
-}else if(age==100){
-    console.log("wow you are wise");
-}
+const title=document.querySelector("#hello h1");
+//only firstone
+//id=# class=.
+console.log(title);
