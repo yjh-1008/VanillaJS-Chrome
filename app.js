5017dc637272fbd6d900888e265ca7ac1e44d6fd
yjh-1008 <71313511+yjh-1008@users.noreply.github.com>
2021-08-15 02:22:31 +0900

    eventListener

--- app.js
+++ app.js
@@ -13,6 +13,26 @@ function handleMouseLeave(){
     title.innerText="Mouse is gone~";
 }
 
+function handleWindowResize(){
+    document.body.style.backgroundColor="tomato";
+}
+
+function handleWindowCopy(){
+    alert("copier");
+}
+
+function handleWindowOffline(){
+    alert("SOS on WIFI");
+}
+
+function handleWindowOnline(){
+    alert("ALL GOOD!");
+}
 title.addEventListener("click",handleTitleClick);
 title.addEventListener("mouseenter",handleMouseEnter);
 title.addEventListener("mouseleave",handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy",handleWindowCopy);
+window.addEventListener("offline",handleWindowOffline);
+window.addEventListener("online",handleWindowOnline);
