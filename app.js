f9307523012987b4f2e46a7713959d6c84506431
yjh-1008 <71313511+yjh-1008@users.noreply.github.com>
2021-08-15 02:59:53 +0900

    class List

--- app.js
+++ app.js
@@ -1,38 +1,7 @@
-const title=document.querySelector("div.hello:first-child h1");
+const h1=document.querySelector("div.hello:first-child h1");
 
-function handleTitleClick(e){
-    title.style.color="blue";
-    console.log("title was licked!");
+function handleTitleClick(){
+    h1.classList.toggle("clicked");
+    
 }
-
-function handleMouseEnter(){
-    title.innerText="Mouse is here";
-}
-
-function handleMouseLeave(){
-    title.innerText="Mouse is gone~";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor="tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier");
-}
-
-function handleWindowOffline(){
-    alert("SOS on WIFI");
-}
-
-function handleWindowOnline(){
-    alert("ALL GOOD!");
-}
-title.addEventListener("click",handleTitleClick);
-title.addEventListener("mouseenter",handleMouseEnter);
-title.addEventListener("mouseleave",handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy",handleWindowCopy);
-window.addEventListener("offline",handleWindowOffline);
-window.addEventListener("online",handleWindowOnline);
+h1.addEventListener("click",handleTitleClick);
