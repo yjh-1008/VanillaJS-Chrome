b7cf028d79a95d13c21a42c84dda1512d7f185ce
yjh-1008 <71313511+yjh-1008@users.noreply.github.com>
2021-08-15 02:04:49 +0900

    mose evnet

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const title=document.querySelector("#hello h1");
-//only firstone
-//id=# class=.
-console.log(title);
+const title=document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick(e){
+    title.style.color="blue";
+    console.log("title was licked!");
+}
+
+function handleMouseEnter(){
+    title.innerText="Mouse is here";
+}
+
+function handleMouseLeave(){
+    title.innerText="Mouse is gone~";
+}
+
+title.addEventListener("click",handleTitleClick);
+title.addEventListener("mouseenter",handleMouseEnter);
+title.addEventListener("mouseleave",handleMouseLeave);
