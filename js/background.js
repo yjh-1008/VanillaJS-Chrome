dbc90d37a553b0a3fb1df438c784395564010422
yjh-1008 <71313511+yjh-1008@users.noreply.github.com>
2021-08-15 21:56:05 +0900

    background images

---
+++ js/background.js
@@ -0,0 +1,11 @@
+const images=[
+    "0.jpeg",
+    "1.jpeg",
+    "2.jpeg"
+]
+
+const chosenImage=images[Math.floor(Math.random()*images.length)];
+const bgImage=document.createElement("img");
+bgImage.src=`image/${chosenImage}`;
+document.body.appendChild(bgImage);
+document.body.baseURI(bgImage);
